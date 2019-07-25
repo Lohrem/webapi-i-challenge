cb8ccb5b11426e864025c5d628c68bac66dce0ee
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-07-25 03:53:07 -0400

    included validation code

--- index.js
+++ index.js
@@ -16,57 +16,79 @@ server.get('/api/users', (req, res) => {
 
 server.get('/api/users/:id', (req, res) => {
   const userID = req.params.id
-  db.findById(userID)
-    .then(user => {
-      res.status(200).json(user)
-    }).catch(err => {
-      res.status(500).json({ success: false, err })
-    })
+  if(!userID) {
+    console.log("could not find ", userID)
+    res.status(404).json({ errorMessage: "user with that ID doesn't exist" })
+  }
+  else {
+    db.findById(userID)
+      .then(user => {
+        res.status(200).json(user)
+      }).catch(err => {
+        res.status(500).json({ success: false, err })
+      })
+  }
 })
 
 server.post('/api/users', (req, res) => {
-  const userInfo = req.body
-  db.insert(userInfo)
-    .then(user => {
-      res.status(201).json({ success: true, user })
-    })
-    .catch(err => {
-      res.status(500).json({ success: false, err })
-    })
+  const { name, bio } = req.body
+  if(!name || !bio) {
+    res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+  }
+  else{
+    db.insert({name, bio})
+      .then(user => {
+        console.log("added: ", user)
+        res.status(201).json({ success: true, user })
+      })
+      .catch(err => {
+        res.status(500).json({ success: false, err })
+      })
+  }
 })
 
 server.delete('/api/users/:id', (req, res) => {
   const userID= req.params.id
-  db.remove(userID)
-    .then(deleted => {
-      if(deleted) {
-        res.status(204).end()
-      }
-      else {
-        res.status(404).json({ success: false, msg: "try again"})
-      }
-    })
-    .catch(err => {
-      err.status(500).json({ success: false, err })
-    })
+  if(!userID) {
+    res.status(404).json({ errorMessage: "mate with that ID doesn't exist" })
+  }
+  else {
+    db.remove(userID)
+      .then(deleted => {
+        if(deleted) {
+          res.status(204).end()
+        }
+        else {
+          res.status(404).json({ success: false, msg: "try again"})
+        }
+      })
+      .catch(err => {
+        err.status(500).json({ success: false, err })
+      })
+  }
 })
 
 server.put('/api/users/:id', (req, res) => {
-  const userID = req.params.id
+  const id = req.params.id
   const userInfo = req.body
-  db.update(userID, userInfo)
-    .then(updated => {
-      if(updated) {
-        res.status(200).json({ success: true, updated})
-        console.log("updated: ", userID, userInfo)
-      }
-      else {
-        res.status(404).json({ success: false, msg: "nuh uh"})
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ success: false, err})
-    })
+  if(!id) {
+    res.status(404).json({ errorMessage: "mate with that ID doesn't exist" })
+  }
+  else {
+    db.update(id, userInfo)
+      .then(updated => {
+        if(updated) {
+          res.status(200).json({ success: true, updated})
+          console.log("updated: ", id, userInfo)
+        }
+        else {
+          res.status(404).json({ success: false, msg: "nuh uh"})
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ success: false, err})
+      })
+  }
 })
 
 server.listen(3000, () => {
